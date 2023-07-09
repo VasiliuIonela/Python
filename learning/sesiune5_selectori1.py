d11ef54131e6dd5a94d8cdbfbfc9613eb60ef34f
Ionela Vasiliu <paveliuc.ionela@yahoo.com>
2023-07-09 07:16:05 +0300

    new commit

---
+++ learning/sesiune5_selectori1.py
@@ -0,0 +1,4 @@
+#selector= sir de caractere care are rolul de a identifica unul sau mai multe elem intr-o pag
+# ex:   ID- identificator unic
+#       Class-
+#       Link Text, Partial Link Text, XPATH, CSS Selector
